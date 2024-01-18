1f9ad5d7d187dda3a71d1cb50ff189b6ea0708e8
Jore Averbeck <noa.averbeck@gmail.com>
2024-01-18 14:49:05 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -21,26 +21,19 @@ export default function Home() {
     const formData = new FormData(event.target);
     const todoData = Object.fromEntries(formData);
 
-    //das ist nun neu:
-    //Ein POST-Anfrage wird an die Server-API (/api/products) gesendet,
-    //um das neue Produkt hinzuzufügen. Die Daten werden als JSON gesendet.
     const response = await fetch("/api/todos", {
       method: "POST",
       body: JSON.stringify(todoData),
       headers: { "Content-Type": "application/json" },
     });
-    //hier checken wir nur nochmal ob die response ok ist:
+
     if (response.ok) {
-      //Wenn die Serverantwort in Ordnung ist (response.ok), wird die Funktion mutate aufgerufen,
-      //um die Daten neu abzurufen und die Benutzeroberfläche zu aktualisieren.
       mutate();
     }
 
     event.target.reset();
   }
 
-  // DELETE Todo on the main page
-  // DELETE Todo on the main page
   async function handleDeleteTodo(id) {
     const response = await fetch(`/api/todos/${id}`, {
       method: "DELETE",
